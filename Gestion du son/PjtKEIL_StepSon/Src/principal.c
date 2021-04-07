73bfabaa41cf58512ea8cedb5667dbe02bc23418
Hugo Barral <hugo.barral@outlook.fr>
2021-04-07 15:15:24 +0200

    Gestion du son - Etape 4.3.2

--- Gestion du son/PjtKEIL_StepSon/Src/principal.c
+++ Gestion du son/PjtKEIL_StepSon/Src/principal.c
@@ -20,14 +20,18 @@ CLOCK_Configure();
 // Configuration du Timer 4 en fonction de la période en micro seconde
 // récupérée dans bruitverre.asm
 Timer_1234_Init_ff(TIM4, 72*PeriodeSonMicroSec);
-
+// PWM de fréquence 100kHz
+PWM_Init_ff(TIM3, 3, 720);
+	
 // On ajoute la débordement (priorité 2) sur la fonction CallbackSon de GestionSon.s
 Active_IT_Debordement_Timer(TIM4, 2, CallbackSon);
-
 	
+// Liaison du canal 3 à la pin PB.0
+GPIO_Configure(GPIOB, 0, OUTPUT, ALT_PPULL);
+
 //============================================================================	
 	
 	
-while	(1);
+while	(1)	PWM_Set_Value_TIM3_Ch3(SortieSon);
 }
 
